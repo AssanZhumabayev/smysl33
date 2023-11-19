ac46f82a4d91e2f5d168743c448af38be43001a4
AssanZhumabayev <141062605+AssanZhumabayev@users.noreply.github.com>
2023-11-19 20:21:49 +0500

    added static root

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -48,8 +48,8 @@ class BasicInstalltest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
-        header = self.browser.find_element(By.TAG_NAME, "h1")
-        self.assertTrue(header.location['x'] > 10
+        footer = self.browser.find_element(By.CLASS_NAME, "footer")
+        self.assertTrue(footer.location['y'] > 600
         )
 
     def test_home_page_blog(self):
